430f02825a19cc7f356e6b2bdfe4e5f9a64580d7
Claribel Aguilar <claribel.aguilarwhyte@gmail.com>
2018-07-07 16:01:39 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -8,5 +8,12 @@ attr_accessor :emails
 def initialize(emails)
   @emails = emails
   end
-  
+
+  def parse
+    # split into an array using the regex of "possible comma followed by white space"
+    # pick only from the array only those emails that are not blank spaces
+    # select only those unique emails
+
+    emails.split(/[,?\s]/).select{|email| email != ""}.uniq
+  end
 end
